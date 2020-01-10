3504293a8adbbb623e44c0d4713e76d710e6bcf8
hewenjun-jk <hewenjun-jk@360jinrong.net>
2020-01-10 18:14:52 +0800

    build: resource

---
+++ build/precache-manifest.487d5c1d9f98acb258793f262ed5af9a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa9d2068841b059fa2fdd11755a8f083",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b15e0009a74e9579cc9",
+    "url": "/static/css/2.18e26356.chunk.css"
+  },
+  {
+    "revision": "301d108cff6c01c7371b",
+    "url": "/static/css/main.e70c85d0.chunk.css"
+  },
+  {
+    "revision": "2b15e0009a74e9579cc9",
+    "url": "/static/js/2.8b0863a3.chunk.js"
+  },
+  {
+    "revision": "4aa0fe1eaee3dcf45338a38641d40f08",
+    "url": "/static/js/2.8b0863a3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "301d108cff6c01c7371b",
+    "url": "/static/js/main.7e98c74d.chunk.js"
+  },
+  {
+    "revision": "a9c6edfe900e04010cf5",
+    "url": "/static/js/runtime-main.c5268d1b.js"
+  }
+]);
